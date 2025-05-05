2b59ddea0347558944b3fc4680c7236bb9418c28
Valeriia Yavdiuk <yavdyuk@gmail.com>
2025-05-05 09:14:20 +0200

    Add files via upload

---
+++ texts.js
@@ -0,0 +1,86 @@
+const pageTexts = {
+    index: {
+      subHeaderTitle: "Journal Home Lite creation",
+      title: "Select journal",
+      subtitle: "Select the journal that you want to claim",
+      searchLabel: "Journal *",
+      searchPlaceholder: "Enter ISSN",
+      searchError: "Select a journal",
+      continueIndexButton: "Continue"
+    },
+    confirmJournal: {
+      title: "Is this your journal?",
+      confirmationText: "I hereby confirm that I work for this journal and have right to claim journal page on their behalf",
+      continueConfirmButton: "Log in to claim"
+    },
+
+    login: {
+      loginTitle: "Log in to claim your journal",
+      emailLabel: "Email *",
+      emailPlaceholder: "Enter your email",
+      passwordLabel: "Password *",
+      passwordPlaceholder: "Enter your password",
+      keepLoggedInLabel: "Keep me logged in",
+      loginButton: "Log in",
+      forgotPasswordHint: '<a href="https://www.researchgate.net/application.LostPassword.html" class="signup-link">Forgot password?</a>',
+      signupPrompt: 'Don’t have an account? <a href="#" class="signup-link" id="switchToSignup">Sign up</a>'
+    },
+    signup: {
+      authTitle: "Create your account",
+      firstNameLabel: "First name *",
+      lastNameLabel: "Last name *",
+      signupEmailLabel: "Journal email *",
+      signupEmailHint: "Use your journal email address",
+      signupPasswordLabel: "Password *",
+      signupCheckboxLabel: "I agree to Business service terms",
+      createAccountButton: "Create account",
+      signinPrompt: 'Already have an account? <a href="#" class="signup-link" id="switchToLogin">Sign in</a>'
+    },
+
+    confirmEmail: {
+      heading: "Check your inbox",
+      line1: "We’ve sent an email to <b>test@journal.com.</b>",
+      line2: "Enter the code from the email to continue.",
+      codePlaceholder: "Enter the code from your email",
+      continueButton: "Continue",
+      spamHint: "Didn’t get the email? Check your spam folder.",
+      spamHintPage: 'Didn’t get the email? Check your spam folder or <a href="#" class="signup-link" id="switchToLogin">start over.</a>'
+    },
+
+    additionalInfo: {
+      title: "Confirm your affiliation with the journal",
+      subtitle: "Provide your business email to help us confirm your affiliation with <b>Nature Sciences</b>",
+      additionalEmailLabel: "Journal email *",
+      additionalEmailInput: "Enter your email",
+      additionalEmailHint: "Use your journal email address",
+      infoLabel: "Additional information",
+      infoTextarea: "E.g. a link to the journal’s website",
+      continueButton: "Continue",
+      infoText: "Provide us with additional information that will help us confirm your affiliation."
+    },
+
+    checkout: {
+      subHeaderTitle: "Checkout",
+      checkoutSubtitle: "Review your order and proceed to payment",
+      billingHeader: "Billing information",
+      creditCardDetails: "Credit card details",
+      orderReview: "Review your order",
+      continueButton: "Continue",
+      continueButton2: "Continue",
+      purchaseButton: "Purchase subscription",
+      labelCompany: "Company/Institution *",
+      labelDepartment: "Department",
+      labelFirstName: "First name *",
+      labelLastName: "Last name *",
+      labelAddress: "Address *",
+      labelCountry: "Country *",
+      labelCity: "City *",
+      labelState: "State/Province/Region",
+      labelPostalCode: "Postal code",
+      summaryHeader: "Summary"
+    }
+    
+    
+    
+  };
+  
